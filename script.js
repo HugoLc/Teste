b95a7aa9a24ad458fde1c3fc91dbb8c42b17fe92
hugolc <hugolindoso.c@gmail.com>
2021-07-22 18:13:32 -0300

    Alterada a apresentação do slider para autoplay e fade

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-$('.carrossel').slick();
+$('.carrossel').slick({
+  // autoplay
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  // fade
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
